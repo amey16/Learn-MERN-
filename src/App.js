9c44318969bf878b31da8e36f776856e7444c609
amey16 <ameyagupta19@gmail.com>
2021-08-20 14:18:25 +0530

    first-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from "./Header";
+ 
+function App() {
+  return (
+
+    <div className="app">
+      {/* <hi>Lets build the MERN TINDER clone!!!</hi> */}
+
+      {/* top bar */}
+       <Header />
+      {/* photo cards */}
+      {/* swipe buttons */}
+    </div>
+  );
+}
+
+export default App;
